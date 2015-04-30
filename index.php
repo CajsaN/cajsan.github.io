e6dda9a19dc53d8f32ffec5ef80feab1ec93e7d8
CajsaN <CajsaN@users.noreply.github.com>
2015-04-30 00:52:51 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<!--detta är ? -->
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="author" content="Cajsa Nolskog">
+		<meta name="description" content="Assignment Web design product Flyme doormat">
+		<meta name="keywords" content="Flyme Doormat, Final assignment web design class">
+		<meta charset="UTF-8">
+		
+		
+		<style>
+		/* Sticky navbar styles
+		-------------------------------------------------- */
+		body {	  
+/*		  padding-top: 70px;*/
+		}
+		
+		/* Sticky footer styles
+		-------------------------------------------------- */
+		html {
+		  position: relative;
+		  min-height: 100%;
+		}
+		body {
+		  /* Margin bottom by footer height */
+		  margin-bottom: 60px;
+		}
+		
+		</style>
+		
+		<title>Flyme Doormat</title>
+		
+		<!-- Latest compiled and minified CSS --> <!--detta är alltså en extern css som någon annan host, sparar tid om populär cache redan pa deras dator content delivery netowork. ordning viktig, min custom är sist och overrides the rest -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		
+		<!--HÄR FINNS FRIA VECTOR: LADDA DENNA FONT AWESOME --><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="flymedoormat.css">
+	</head>
+	
+	
+	
+	<!-- HÄR BÖRJAR BODY!!!!!!!!!!!!! -->
+	
+	<body data-spy="scroll" data-target="#navbar">
+		
+	<!-- Fixed navbar -->
+	    <nav class="navbar navbar-default navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+			  <!-- <img alt="Socks!" src="img/ylogo.png"> -->
+	          <a class="navbar-brand" href="flymedoormat.html">Flyme Doormat</a>
+	        </div>
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <!-- <ul class="nav navbar-nav">
+	            <li class=""><a href="#">Home</a></li>
+	            <li><a href="#about">About</a></li>
+	            <li><a href="#contact">Contact</a></li>
+	            <li class="dropdown">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+	              <ul class="dropdown-menu" role="menu">
+	                <li><a href="#">Action</a></li>
+	                <li><a href="#">Another action</a></li>
+	                <li><a href="#">Something else here</a></li>
+	                <li class="divider"></li>
+	                <li class="dropdown-header">Nav header</li>
+	                <li><a href="#">Separated link</a></li>
+	                <li><a href="#">One more separated link</a></li>
+	              </ul>
+	            </li>
+	          </ul> -->
+	          <ul class="nav navbar-nav navbar-right">
+	             
+				 
+				 <li><a href="features.html">Features</a></li>
+					 <li><a href="history.html">History</a></li>
+					 <li><a href="flymecontact.html">Contact</a></li>
+					  <li><a href="aboutus.html">About Us</a></li>
+				
+	            <!-- <li><a href="../navbar-static-top/"></a></li> -->
+	            <li class=""><a href="faq.html">FAQ</a></li>
+			<!--	<li><a href="purchasepage.html"><button type="button" class="btn btn-blue navbar-btn">Buy Now</button></a></li> -->
+				  <li><button type="button" class="btn btn-default navbar-btn"><a href="purchasepage.html">Buy Now</a></button></li>
+	          </ul>
+			
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+		<div id="submitpage"> <!-- 962954 "container"-->
+			<div class="container">
+		
+				<div class="row">
+					<div class="col-xs-12">
+		<h2 class="text-center"> Thank you for your question, we'll be in touch shortly! </h2>
+			</div> <!--column -->
+		</div>  <!--row -->
+			</div> 	<!--container -->	
+		</div><!-- .submit -->
+		
+			
+					<footer class="footer">
+						<div class="container">
+							<div class="row">
+						
+	 				
+						
+								<div class="text-center"> <div class="col-sm-12">
+						  <br>  <br>
+									<a href="#" class="btn btn-orange"><i class="fa fa-twitter"></i>Twitter</a>
+								<a href="#" class="btn btn-orange"><i class="fa fa-facebook"></i>Facebook</a>
+								<a href="mailto:flymedoormat@flymedoormat.com" class="btn btn-orange"><i class="fa fa-envelope-o"></i>Electronic mail</a>
+		    </div><!--column -->
+		</div> <!--class center..? -->
+			</div> <!-- row -->
+				  
+ 		   
+					 <br>  <br>
+					</div> <!--container -->
+					
+				    </footer>
+		
+						<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+						    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+							<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	</body> </html>
